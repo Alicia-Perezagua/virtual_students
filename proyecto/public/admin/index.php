5cd3115c3d6fca7bae8f922ad721bccf0cc31087
PredatorMaster <78963090+PredatorMaster@users.noreply.github.com>
2022-06-13 18:42:53 +0200

    modulos_usuarios y admin

--- proyecto/public/admin/index.php
+++ proyecto/public/admin/index.php
@@ -1,5 +1,5 @@
 <?php include 'header.php'; ?>
-<divclass="index-body p-0 m-0">
+<divclass="index-body p-0 m-0>
   <div class="hero container-fluid d-flex justify-content-center align-items-center p-0 m-0">
     <div class="transparencia p-0 m-0 h-100 w-100 d-flex justify-content-center align-items-center">
       <div class="contenedor-titulo container p-0 m-0 d-flex justify-content-center align-items-center">
